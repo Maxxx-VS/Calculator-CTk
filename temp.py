00740337689f04423925115738ba9bb55972d35b
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-18 22:34:36 +0600

    Возвращает result, не возвращает calc

--- temp.py
+++ temp.py
@@ -9,34 +9,72 @@ root.geometry("575x425")
 root.resizable(False, False)
 root.iconbitmap('calculator.ico')
 
+sum_2 = count = 0
+oper = tbt = ""
 sum_1 = ""
-count = 0
+def operators(op):
+    global sum_1
+    global sum_2
+    global oper
+    if op == "+":
+        sum_2 = sum_1
+        sum_1 = 0
+        oper = "+"
+    elif op == "-":
+        sum_2 = sum_1
+        sum_1 = 0
+        oper = "-"
+    elif op == "*":
+        sum_2 = sum_1
+        sum_1 = 0
+        oper = "*"
+    elif op == "/":
+        sum_2 = sum_1
+        sum_1 =0
+        oper = "/"
+    else:
+        sum_2 = sum_1
+        sum_1 = 0
+        oper = "%"
 
 def numbers(numb):
-    global sum_1
     global count
-    if count == 0:
-        sum_1 += str(numb)
-        textbox_1.insert("end", numb)
-        textbox_2.delete(0.0, "end")
-    else:
-        count = sum_1
-        sum_1 += numb
-        textbox_1.insert("end", count)
-        textbox_2.delete(0.0, "end")
+    global sum_1
+    global tbt
+    sum_1 += str(numb)
+
+    textbox_1.insert("end", numb)
+
+    print(sum_1)
+
+    print(numb)
+
 
 def ecuals():
+    global oper
     global sum_1
     global count
-    count = eval(sum_1)
+    if oper == "+":
+        count = sum_1 + sum_2
+    elif oper == "-":
+        count = sum_1 - sum_2
+    elif oper == "*":
+        count = sum_1 * sum_2
+    elif oper == "/":
+        count = sum_1 / sum_2
+    else:
+        count = (sum_1 / 100) * sum_2
     textbox_1.delete(0.0, "end")
     textbox_2.insert("end", count)
 
 def acc():
     global sum_1
+    global sum_2
     global count
-    sum_1 = ""
-    count = 0
+    global oper
+    global tbt
+    sum_1 = sum_2 = count = 0
+    oper = tbt = ""
     textbox_1.delete(0.0, "end")
     textbox_2.delete(0.0, "end")
 
@@ -51,11 +89,11 @@ button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers(7
 button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers(8)), text="8").grid(row=3, column=2, padx=10, pady=10)
 button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers(9)), text="9").grid(row=4, column=2, padx=10, pady=10)
 button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers(0)), text="0").grid(row=5, column=0, padx=10, pady=10)
-button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers('%')), text="%").grid(row=5, column=1, padx=10, pady=10)
-button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers('/')), text="/").grid(row=5, column=2, padx=10, pady=10)
-button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers('+')), text="+").grid(row=2, column=3, padx=10, pady=10)
-button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers('-')), text="-").grid(row=3, column=3, padx=10, pady=10)
-button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (numbers('*')), text="*").grid(row=4, column=3, padx=10, pady=10)
+button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (operators('%')), text="%").grid(row=5, column=1, padx=10, pady=10)
+button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (operators('/')), text="/").grid(row=5, column=2, padx=10, pady=10)
+button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (operators('+')), text="+").grid(row=2, column=3, padx=10, pady=10)
+button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (operators('-')), text="-").grid(row=3, column=3, padx=10, pady=10)
+button = ctk.CTkButton(root, height = 10, width = 75, command=lambda: (operators('*')), text="*").grid(row=4, column=3, padx=10, pady=10)
 button = ctk.CTkButton(root, fg_color='green', height = 10, width = 75, command=ecuals, text="=").grid(row=5, column=3, padx=10, pady=10)
 label = ctk.CTkLabel(root, text=f"CALC:").grid(row=0, column=0, padx=10, pady=10)
 textbox_1 = ctk.CTkTextbox(root, height = 10, width = 75)
